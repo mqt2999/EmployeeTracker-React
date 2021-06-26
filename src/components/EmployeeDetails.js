bc8bdc0b0a702419ba73cb5fad113e84b5fcf49e
Marquell Thompson <77359365+mqt2999@users.noreply.github.com>
2021-06-25 22:26:50 -0500

    adding each row to the table

---
+++ src/components/EmployeeDetails.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function EmpRow () {
+    return (
+        <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+      <td> hello</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+    )
+}
+export default EmpRow;
