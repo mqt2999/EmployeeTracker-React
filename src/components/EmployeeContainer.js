232d3890b9f651e9cc9d32d450a63dbcf04d342a
Marquell Thompson <77359365+mqt2999@users.noreply.github.com>
2021-06-27 18:15:11 -0500

    thrying to add the sort button net to the name

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -5,20 +5,20 @@ const styles ={
         marginTop: "5%",
     }
 }
-function EmpContainer () {
+function EmpContainer (props) {
     return (
         <div style={styles.space}>
         <table class="table">
   <thead>
     <tr>
       <th scope="col">Image</th>
-      <th scope="col">Name</th>
+      <th className="pb-2" scope="col">Name   <button className='btn btn-sm px-0' onClick={props.handleSort}><i className="bi bi-arrow-down-up"></i></button></th>
       <th scope="col">Phone</th>
       <th scope="col">Email</th>
       <th scope="col">DOB</th>
     </tr>
   </thead>
-  <EmpRow/>
+  <EmpRow {...props}/>
 </table>
 </div>
     )
