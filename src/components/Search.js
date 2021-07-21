820418bcac8e2621132adb33e8bef76fa7cf1843
Marquell Thompson <77359365+mqt2999@users.noreply.github.com>
2021-07-20 19:03:31 -0500

    final push

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,23 +1,23 @@
 import React from "react";
 
-const styles ={
-    center:{
-        textAlign:"center"
+const styles = {
+    center: {
+        textAlign: "center"
     }
 }
-function Search(props){
+function Search(props) {
     return (
         <form className="form-inline d-flex justify-content-center">
-          <input 
-          value={props.Search}
-          onChange={e => props.handleFormSubmit(e)}
-          name="search"
-          type="search" 
-          placeholder="Search" 
-          aria-label="Search" 
-          />
+            <input
+                value={props.Search}
+                onChange={e => props.handleFormSubmit(e)}
+                name="search"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+            />
         </form>
-        )
+    )
 }
 
 export default Search;
